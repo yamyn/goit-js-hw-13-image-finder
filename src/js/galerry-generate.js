d1b1fca74293dabd51b92d77013cd8f9f3401342
Yaroslav Myn <ricky.month@gmail.com>
2020-01-30 11:22:19 +0200

    Refactoring app

--- src/js/galerry-generate.js
+++ src/js/galerry-generate.js
@@ -1,32 +1,61 @@
 import InfiniteScroll from 'infinite-scroll';
 import galleryCartTemplate from '../templates/gallery-cart-template.hbs';
+import imagesService from './services/apiService';
 
 const refs = {
+  form: document.querySelector('#search-form'),
   gallery: document.querySelector('#gallery'),
 };
 
-const infScrollInstance = new InfiniteScroll(refs.gallery, {
-  responseType: 'text',
-  history: false,
-  path() {
-    return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=14998986-82322fa46abf8765da09830ba&q=yellow+flowers&image_type=photo&pretty=true&page=${this.pageIndex}`;
-  },
-});
+refs.form.addEventListener('submit', getImagesHandler);
 
-infScrollInstance.on('load', response => {
-  const imagesObj = JSON.parse(response);
-  const images = imagesObj.hits;
-  console.log(images);
-  const markup = images.reduce(
-    (string, image) => string + galleryCartTemplate(image),
-    '',
-  );
+function getImagesHandler(event) {
+  event.
+}
 
-  const proxyEl = document.createElement('div');
-  proxyEl.innerHTML = markup;
+// const infScrollInstance = new InfiniteScroll(refs.gallery, {
+//   responseType: 'text',
+//   history: false,
+//   path() {
+//     return '';
+//   },
+// });
 
-  console.log(proxyEl);
-  infScrollInstance.appendItems(proxyEl.children);
-});
+// infScrollInstance.on('load', response => {
+//   const imagesObj = JSON.parse(response);
+//   const images = imagesObj.hits;
+//   console.log(images);
+//   const markup = images.reduce(
+//     (string, image) => string + galleryCartTemplate(image),
+//     '',
+//   );
+//   const proxyEl = document.createElement('div');
+//   proxyEl.innerHTML = markup;
 
-infScrollInstance.loadNextPage();
+//   console.log(proxyEl);
+//   infScrollInstance.appendItems(proxyEl.children);
+// });
+
+// refs.form.addEventListener('submit', getImageHandler);
+
+// function getImageHandler(form) {
+//   infScrollInstance.destroy();
+//   refs.gallery.innerHTML = '';
+
+//   form.preventDefault();
+//   console.log(form);
+
+//   const searchQuery = form.currentTarget.elements.query.value;
+//   console.log(searchQuery);
+
+//   infScrollInstance.pageIndex = 1;
+//   infScrollInstance.option({
+//     path() {
+//       return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=14998986-82322fa46abf8765da09830ba&q=${searchQuery}&page=${this.pageIndex}`;
+//     },
+//   });
+
+//   infScrollInstance.loadNextPage();
+// }
+
+// cl;
---
+++ src/js/services/apiService.js
@@ -0,0 +1,13 @@
+const corsServ = 'https://cors-anywhere.herokuapp.com/';
+const baseUrl = 'https://pixabay.com/api/';
+const apiKey = '?key=14998986-82322fa46abf8765da09830ba';
+export default {
+  page: 1,
+  fethWeather(searchQuery) {
+    const query = `&q=${searchQuery}`;
+    const currentPage = `&page=${this.page}`;
+    return fetch(baseUrl + apiKey + query + currentPage)
+      .then(response => response.json())
+      .then(data => data.list);
+  },
+};
