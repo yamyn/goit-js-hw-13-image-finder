84e4f0b6364bad6cbc4fdb27427aa62813fe6ce5
Ya_Myn <ricky.month@gmail.com>
2020-02-17 19:21:01 +0200

    Fixed scroll

--- src/js/galerry-generate.js
+++ src/js/galerry-generate.js
@@ -41,10 +41,6 @@ function searchFormSubmitHandler(event) {
 
 function loadMoreBtnHandler() {
     toGetImages();
-    scroolToNewImage();
-}
-
-function scroolToNewImage() {
     const heightOfScroll = window.innerHeight - 200;
     setTimeout(() => {
         window.scrollBy(0, heightOfScroll);
@@ -74,13 +70,6 @@ function toGetImages() {
     });
 }
 
-// function addedSizer() {
-//   const sizer = document.createElement('li');
-//   sizer.classList.add('item');
-//   sizer.classList.add('gallery__sizer');
-//   refs.gallery.appendChild(sizer);
-// }
-
 function toGenMarkup(images) {
     const markup = images.map(image => galleryCartTemplate(image));
 
@@ -99,50 +88,3 @@ function clearGalleryList() {
     masonryInstance.remove(refs.gallery.children);
     masonryInstance.layout();
 }
-
-// const infScrollInstance = new InfiniteScroll(refs.gallery, {
-//   responseType: 'text',
-//   history: false,
-//   path() {
-//     return '';
-//   },
-// });
-
-// infScrollInstance.on('load', response => {
-//   const imagesObj = JSON.parse(response);
-//   const images = imagesObj.hits;
-//   console.log(images);
-//   const markup = images.reduce(
-//     (string, image) => string + galleryCartTemplate(image),
-//     '',
-//   );
-//   const proxyEl = document.createElement('div');
-//   proxyEl.innerHTML = markup;
-
-//   console.log(proxyEl);
-//   infScrollInstance.appendItems(proxyEl.children);
-// });
-
-// refs.form.addEventListener('submit', getImageHandler);
-
-// function getImageHandler(form) {
-//   infScrollInstance.destroy();
-//   refs.gallery.innerHTML = '';
-
-//   form.preventDefault();
-//   console.log(form);
-
-//   const searchQuery = form.currentTarget.elements.query.value;
-//   console.log(searchQuery);
-
-//   infScrollInstance.pageIndex = 1;
-//   infScrollInstance.option({
-//     path() {
-//       return `https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=14998986-82322fa46abf8765da09830ba&q=${searchQuery}&page=${this.pageIndex}`;
-//     },
-//   });
-
-//   infScrollInstance.loadNextPage();
-// }
-
-// cl;
